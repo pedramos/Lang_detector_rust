bbb1498d7d47093d7f6efe9500f3eb71c662f536
Pedro Ramos <rami@opensuse.site>
2015-07-19 17:52:41 +0100

    Started read_line function [not tested ]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn read_file(data: &std){
+
+    println!("Reading file {}",data);
+
+
+    //CENAS sacadas da documentação. Falta adaptar
+    let mut f = try!(File::open("foo.txt"));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    assert_eq!(s, "Hello, world!");
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
