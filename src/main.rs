20be75e55aebfb92ff5104ba8ca606168716eb8b
Pedro Ramos <rami@opensuse.site>
2015-07-25 22:43:53 +0100

    Finally it started working with the text.txt file I made. I should add error handling code for learning proposes

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-fn read_file(data: &std){
+use std::io::BufReader;
+use std::fs::File;
+use std::io::BufRead;
+
+
+fn read_file(data: &str){
 
     println!("Reading file {}",data);
 
+    let  file = File::open("/home/rami/cool_stuff/Lang_detector_rust/src/test.txt");
 
-    //CENAS sacadas da documentação. Falta adaptar
-    let mut f = try!(File::open("foo.txt"));
-    let mut s = String::new();
-    try!(f.read_to_string(&mut s));
-
-    //CEnas melhores para ler ficheiros
-    use std::io::{BufReader,BufRead};
-    use std::fs::File;
-
-fn main() {
-    let file = File::open("file.txt").unwrap();
-    for line in BufReader::new(file).lines() {
+    let buf = BufReader::new(file.unwrap());
+    for line in buf.lines() {
         println!("{}", line.unwrap());
     }
-}
+
 
 }
 
 fn main() {
-    println!("Hello, world!");
+    read_file("test.txt")
 }
