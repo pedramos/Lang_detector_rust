1fed88a9c4de2b1e31301f6ae69b5d4fd668fabb
Pedro Ramos <rami@opensuse.site>
2015-08-01 23:25:49 +0100

    esta coisa nao funciona porque???????????????????'

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,35 @@ use std::collections::HashMap;
 
 fn read_file(data: &str){
 
-    println!("Reading file {}",data);
+    println!("Reading file {}","/home/rami/cool_stuff/Lang_detector_rust/src/test.txt");
 
     let  file = File::open(data);
 
     let buf = BufReader::new(file.unwrap());
-    let mut hash = HashMap::new();
+    //let mut hash = HashMap::new();
 
     let mut split_1 = String::new();
     let mut split_2 = String::new();
     for line in buf.lines() {
-        split_1.push_str(line.unwrap().split(" ").nth(0).unwrap());
-        split_2.push_str(line.unwrap().split(" ").nth(1).unwrap());
-        println!("{}",split_1);
-        println!("{}",split_2);
-        hash.insert(split_1,split_2.parse::<u32>().unwrap());
-        split_1.clean();
-        split_2.clean();
+        let mut splited = line.unwrap().split(" ");
+        //println!("{}", line.unwrap().split(" ").nth(0).unwrap());
+        println!("{}", splited.nth(1).unwrap());
+        //split_1.push_str(line.unwrap().split(" ").nth(0).unwrap());
+        //split_2.push_str(line.unwrap().split(" ").nth(1).unwrap());
+        //println!("{}",split_1);
+        //println!("{}",split_2);
+        //hash.insert(split_1,split_2.parse::<u32>().unwrap());
+        //split_1.clear();
+        //split_2.clear();
+
+
     }
-
-
-
 }
+
+
+
+
+
 
 fn main() {
     read_file("/home/rami/cool_stuff/Lang_detector_rust/src/test.txt")
