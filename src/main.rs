31ff9b3cbd49a9aae1174b6b25965314d3064bdd
Pedro Ramos <rami@opensuse.site>
2015-08-29 16:51:44 +0100

    Started create trip function. Still incomplete

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ use std::io::BufRead;
 use std::collections::HashMap;
 
 
+
 fn read_file(data: &str) -> HashMap<String,u32> {
 
     println!("Reading file {}",data);
 
     let  file = File::open(data);
     assert!(file.is_ok(), "Couldn't open file");
+
     let buf = BufReader::new(file.unwrap());
-
-let mut hash = HashMap::new();
-
+    let mut hash = HashMap::new();
     let mut split_1 = String::new();
     let mut split_2 = String::new();
+
     for line in buf.lines() {
         assert!(line.is_ok(), "falou a ler linha");
         let linha = line.unwrap();
@@ -34,8 +35,21 @@ let mut hash = HashMap::new();
     }
     hash
 }
+//Function to separete the sentence in groups of 3 letters
+fn create_trip (&str input) -> <Vec<String>> {
 
+let X = input.len();
+if (X<3){
+    println!("Please insert a bigger sentence");
+    let mut result = Vec::with_capacity(0);
+    return result;
+}
+let capacity = X - 2;
+let mut result = Vec::with_capacity(capacity);
 
+//IMCOMPLETE: ja está criado vector com o tamanho certo, falta separar as coisas
+
+}
 
 
 
